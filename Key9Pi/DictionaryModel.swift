60c06819b1de01e3649dcb7df58903b161ee7e28
Anna Hua <annajh@umich.edu>
2017-02-14 19:21:58 -0500

    Initial commit

---
+++ Key9Pi/DictionaryModel.swift
@@ -0,0 +1,23 @@
+//
+//  DictionaryModel.swift
+//  Key9Pi
+//
+//  Created by Anna Hua on 2/14/17.
+//  Copyright © 2017 EECS481. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class DictionaryModel {
+//    init() {
+//        let moc = …
+//        let employeesFetch = NSFetchRequest(entityName: "Employee")
+//        
+//        do {
+//            let fetchedEmployees = try moc.executeFetchRequest(employeesFetch) as! [EmployeeMO]
+//        } catch {
+//            fatalError("Failed to fetch employees: \(error)")
+//        }
+//    }
+}
