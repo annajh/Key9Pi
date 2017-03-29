94a1ec6166c4b5c3e465cc46a686f33ce6150e01
Michael Bremerkamp <mbrem@umich.edu>
2017-03-29 17:50:18 -0400

    added color customization backend

--- Key9pi/ViewController.swift
+++ Key9pi/ViewController.swift
@@ -44,11 +44,11 @@ class ViewControllerMy: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let userDefaults = UserDefaults(suiteName: "group.k9") {
-            userDefaults.set("default" as AnyObject, forKey: "color")
-            userDefaults.set(2 as AnyObject, forKey: "other")
-            userDefaults.synchronize()
-        }
+//        if let userDefaults = UserDefaults(suiteName: "group.k9") {
+//            userDefaults.set("default" as AnyObject, forKey: "color")
+//            userDefaults.set(2 as AnyObject, forKey: "other")
+//            userDefaults.synchronize()
+//        }
   
 //        wordTextField.layer.borderColor = UIColor.black.cgColor;
 //        wordTextField.layer.borderWidth = 2;
@@ -77,6 +77,31 @@ class ViewControllerMy: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func backgroundPress(_ sender: UIButton) {
+        if let userDefaults = UserDefaults(suiteName: "group.k9") {
+            print(sender.currentTitle!)
+            userDefaults.set(sender.currentTitle! as AnyObject, forKey: "background")
+            userDefaults.synchronize()
+        }
+    }
+    
+    @IBAction func buttonPress(_ sender: UIButton) {
+        if let userDefaults = UserDefaults(suiteName: "group.k9") {
+            print(sender.currentTitle!)
+            userDefaults.set(sender.currentTitle! as AnyObject, forKey: "button")
+            userDefaults.synchronize()
+        }
+    }
+    
+    @IBAction func textPress(_ sender: UIButton) {
+        if let userDefaults = UserDefaults(suiteName: "group.k9") {
+            print(sender.currentTitle!)
+            userDefaults.set(sender.currentTitle! as AnyObject, forKey: "text")
+            userDefaults.synchronize()
+        }
+    }
+    
 }
 
 
